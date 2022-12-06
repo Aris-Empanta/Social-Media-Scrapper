e474f3673ef53294dd6d5d2d6387799fe9a7d4b4
Aris Empanta <eams220891@gmail.com>
2022-12-06 18:12:55 +0200

    Receiving  http request from client configurations

--- server/index.js
+++ server/index.js
@@ -1,9 +1,15 @@
 const express = require("express")
 const app = express()
+const cors = require('cors')
 const port = process.env.PORT || 5000
 
-app.get("/", (req, res) => {
-    res.send("hello world")
+app.use(cors())
+app.use(express.urlencoded({extended: true}));
+app.use(express.json())
+
+app.post("/login", (req, res) => {
+
+        console.log(req.body.user)    
 })
 
 
